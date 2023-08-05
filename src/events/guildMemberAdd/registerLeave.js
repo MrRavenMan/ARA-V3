81ea2e6b89bb8d51abb8cf65a64c2fb83c5ba64e
MrRavenMan <CamiEsfandiari@gmail.com>
2023-08-06 00:05:27 +0200

    Fixed leave message

--- src/events/guildMemberAdd/registerLeave.js
+++ src/events/guildMemberAdd/registerLeave.js
@@ -3,5 +3,5 @@ const { userMention } = require('discord.js');
 
 module.exports = (client, user) => {
     console.log(`${user.tag} has left the server at ${new Date().toISOString()}`);
-    client.channels.cache.get(joinLeaveChannelID).send(`${userMention(user.id)} has joined the server`);
+    client.channels.cache.get(joinLeaveChannelID).send(`${userMention(user.id)} has left the server`);
 };
