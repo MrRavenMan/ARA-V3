eb4de8711dc0cbf75118d81ac024d2d22777c180
MrRavenMan <CamiEsfandiari@gmail.com>
2023-08-06 00:02:34 +0200

    Removed user send

--- src/events/guildMemberAdd/registerJoin.js
+++ src/events/guildMemberAdd/registerJoin.js
@@ -4,5 +4,5 @@ const { userMention } = require('discord.js');
 module.exports = (client, user) => {
     console.log(`${user.tag} has joined the server at ${new Date().toISOString()}`);
     client.channels.cache.get(joinLeaveChannelID).send(`**${userMention(user.id)}** has joined the server`)
-    user.send(joinMessage)
+    // user.send(joinMessage)
 };
